707a047bd668463089131bf1a57c5f89a9dc38f5
GabrielGonçalves <77693978+Gahh-gon@users.noreply.github.com>
2024-01-24 12:27:24 -0300

    Navbar completo + LogoImagem

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom"
+import LogoOneshot from '../../assets/logoOneshot.svg'
+import './style.css'
 export function Navbar(){
     return(
-        <nav>
-            <Link to={'/'}><img src="#" alt="" /></Link>
-            <ul>
+        <nav className="flex justify-between h-[100px] sticky top-0 shadow-lg z-10 items-center ">
+
+                  <Link to={'/'} className=" items-center flex ">
+                    <img src={LogoOneshot} alt="Imagem da lampada de oneshot que muitos tratam como sol " className="w-64 "/>
+                  </Link>
+          
+          
+            <ul className="flex space-x-9 pr-10 text-2xl text-white">
                 <li><Link to={"/characters"}>Personagens</Link></li>
                 <li><Link to={"/downloadPage"}>Baixar</Link></li>
                 <li><Link to={"/GameplayPage"}>Gameplay</Link></li>
