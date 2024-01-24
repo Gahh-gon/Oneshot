ea3c92d551a26a72311aca1e8c186bfdc4a8260a
GabrielGonçalves <77693978+Gahh-gon@users.noreply.github.com>
2024-01-24 13:33:28 -0300

    app com Navbar

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,8 +1,9 @@
 import { Outlet } from "react-router-dom"
+import { Navbar } from "../../components/Navbar"
 export function App(){
     return(
         <main>
-            <nav>Teste</nav>
+            <Navbar/>
         <Outlet/>
         </main>
     )
