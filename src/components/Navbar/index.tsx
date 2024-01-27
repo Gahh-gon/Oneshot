43f84a3a58a2def96a11103f9cbf51d6988a243e
GabrielGonçalves <77693978+Gahh-gon@users.noreply.github.com>
2024-01-27 10:51:35 -0300

    Responsividade NavBar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,14 +3,14 @@ import LogoOneshot from '../../assets/logoOneshot.svg'
 import './style.css'
 export function Navbar(){
     return(
-        <nav className="flex justify-between h-[100px] sticky top-0 shadow-lg z-10 items-center ">
+        <nav className="flex justify-between space-x-5 h-[100px] sticky top-0 shadow-lg z-10 items-center ">
 
                   <Link to={'/'} className=" items-center flex ">
-                    <img src={LogoOneshot} alt="Imagem da lampada de oneshot que muitos tratam como sol " className="w-64 "/>
+                    <img src={LogoOneshot} alt="Imagem da lampada de oneshot que muitos tratam como sol " className="w-48 sm:w-52 md:w-60 lg:w-62 xl:w-72 2xl:80"/>
                   </Link>
           
           
-            <ul className="flex space-x-9 pr-10 text-2xl text-white">
+            <ul className="flex space-x-2 md:space-x-6 lg:space-x-8 pr-10 text-white text-[10px] sm:text-sm md:text-base lg:text-xl xl:text-2xl">
                 <li><Link to={"/characters"}>Personagens</Link></li>
                 <li><Link to={"/downloadPage"}>Baixar</Link></li>
                 <li><Link to={"/GameplayPage"}>Gameplay</Link></li>
